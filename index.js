2ed4396a74afbb6c2b935542f9276cc565a9c663
maicol11 <maicol11ce@live.com>
2022-09-02 19:47:33 -0500

    conectado con puerto

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express")
-
+require('dotenv').config({path: '.env'})
 const app = express()
 
-const port = 3000
+const port = process.env['port']
+
 
 app.listen(port, ()=>{console.log("Connect with the port", port)})
 
-var mongoose = require('mongoose');
+/*var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {
     }).then(db => console.log('conexion exitosa'))
     .catch(err => console.log('error: ', err))
+*/
