ed7a6567091c741e201f4007f46df69b3a951414
maicol11 <maicol11ce@live.com>
2022-09-03 13:05:38 -0500

    Conexión a mongo DB

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = process.env['port']
 
 app.listen(port, ()=>{console.log("Connect with the port", port)})
 
-/*var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {
+var mongoose = require('mongoose');
+mongoose.connect(process.env['MONGO_DB'], {
     }).then(db => console.log('conexion exitosa'))
     .catch(err => console.log('error: ', err))
-*/
